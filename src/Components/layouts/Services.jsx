346d86bd053f14d40bea07628988be1dd69dde1d
Matias Lopez <matias.lopez.renato@gmail.com>
2024-08-08 00:08:25 -0400

    Mejoras en la seccion service

--- src/Components/layouts/Services.jsx
+++ src/Components/layouts/Services.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import CortePhoto from '../../assets/img/corte.webp'
+import Afeitado from '../../assets/img/barbacorte.webp'
+import CuidadoBarba from '../../assets/img/cuidadobarba.webp'
+import Coloracion from '../../assets/img/colorpelo.webp'
 import '../../assets/styles/layout/services.css'
-
 
 
 function Services() {
@@ -9,22 +12,22 @@ function Services() {
             <h2>Nuestros Servicios</h2>
             <div className="service-list">
                 <div className="service-item">
-                    <img src="../../img/haircut.jpg" alt="Corte de Cabello" />
+                    <img src={CortePhoto} alt="Corte de Cabello" />
                     <h3>Corte de Cabello</h3>
                     <p>Descripción del servicio de corte de cabello.</p>
                 </div>
                 <div className="service-item">
-                    <img src="../../img/shave.jpg" alt="Afeitado" />
+                    <img src={Afeitado} alt="Afeitado" />
                     <h3>Afeitado Clásico</h3>
                     <p>Descripción del servicio de afeitado clásico.</p>
                 </div>
                 <div className="service-item">
-                    <img src="../../img/beard-care.jpg" alt="Cuidado de Barba" />
+                    <img src={CuidadoBarba} alt="Cuidado de Barba" />
                     <h3>Cuidado de Barba</h3>
                     <p>Descripción del servicio de cuidado de barba.</p>
                 </div>
                 <div className="service-item">
-                    <img src="../../img/hair-color.jpg" alt="Coloración" />
+                    <img src={Coloracion} alt="Coloración" />
                     <h3>Coloración de Cabello</h3>
                     <p>Descripción del servicio de coloración de cabello.</p>
                 </div>
